3a8079847b46c55b9b186970cdf21aeb772c7c86
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-05-07 14:53:47 -0300

    Biblioteca e primeira classe Pessoa

---
+++ biblioteca.py
@@ -0,0 +1,5 @@
+class Pessoa():
+    def __init__(self,name,weight,age):
+        self.nome=name
+        self.peso=weight
+        self.idade=age
---
+++ main.py
@@ -0,0 +1,5 @@
+from biblioteca import Pessoa
+
+aluno01 = Pessoa("Gustavo",26,80)
+
+print(aluno01)
