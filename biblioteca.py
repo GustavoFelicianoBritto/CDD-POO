443fa848f26e9e5f8b78ad01acab63093a783097
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-05-07 15:10:05 -0300

    Biblioteca e funções apresentar, comer, falar e dormir

--- biblioteca.py
+++ biblioteca.py
@@ -3,3 +3,19 @@ class Pessoa():
         self.nome=name
         self.peso=weight
         self.idade=age
+    def apresentar(self):
+        print(f"Olá, meu nome é {self.nome}, "
+              f"tenho {self.idade} anos, e "
+              f"peso {self.peso} kg")
+
+    def comer(self):
+        print(f"{self.nome} está comendo")
+    def falar(self):
+        print(f"{self.nome} está falando sobre sua idade"
+              f" ({self.idade} anos)")
+    def dormir(self):
+        print(f"{self.nome} foi dormir pelas próximas por 20 horas")
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from biblioteca import Pessoa
 
-aluno01 = Pessoa("Gustavo",26,80)
+aluno01 = Pessoa("Gustavo",80,26)
+aluno02 = Pessoa("Lucas",67,19)
 
-print(aluno01)
+# print(aluno01.nome)
+# print(aluno02.nome)
+
+aluno01.apresentar()
+aluno02.comer()
+aluno02.falar()
+aluno01.dormir()
