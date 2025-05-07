c7a1df9129cdc26160fd16427da72af5c9bea836
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-05-07 16:30:02 -0300

    Atividades, impedimentos e repetições de atividades comendo, dormingo, falando, apresentando

--- biblioteca.py
+++ biblioteca.py
@@ -3,18 +3,66 @@ class Pessoa():
         self.nome=name
         self.peso=weight
         self.idade=age
-    def apresentar(self):
-        print(f"Olá, meu nome é {self.nome}, "
-              f"tenho {self.idade} anos, e "
-              f"peso {self.peso} kg")
+        self.statusDormindo=False
+        self.statusComendo=False
+        self.statusFalando=False
+        self.statusApresentando=False
+        self.statusAtividade=""
 
-    def comer(self):
-        print(f"{self.nome} está comendo")
+    def apresentar(self):
+        if self.statusApresentando==True:
+            print(f"{self.nome} já está se apresentando")
+        elif self.statusDormindo or self.statusFalando or self.statusComendo:
+            print(f"{self.nome} não conseguiu, porque está {self.statusAtividade}")
+        else:
+            self.statusApresentando=True
+            self.statusDormindo = False
+            self.statusComendo = False
+            self.statusFalando = False
+            self.statusAtividade= " se apresentando "
+            print(f"Olá, meu nome é {self.nome}, "
+                  f"tenho {self.idade} anos, e "
+                  f"peso {self.peso} kg")
+
+    def comer(self,comida):
+        if self.statusComendo==True:
+            print(f"{self.nome} já está comendo")
+        elif self.statusDormindo or self.statusFalando or self.statusApresentando:
+            print(f"{self.nome} não conseguiu, porque está {self.statusAtividade}")
+        else:
+            self.statusComendo=True
+            self.statusDormindo = False
+            self.statusFalando = False
+            self.statusApresentando = False
+            print(f"{self.nome} está comendo {comida}")
+            self.statusAtividade = " comendo "
+
     def falar(self):
-        print(f"{self.nome} está falando sobre sua idade"
-              f" ({self.idade} anos)")
+        if self.statusFalando==True:
+            print(f"{self.nome} já está falando")
+        elif self.statusDormindo or self.statusComendo or self.statusApresentando:
+            print(f"{self.nome} não conseguiu, porque está {self.statusAtividade}")
+        else:
+            self.statusComendo=False
+            self.statusDormindo = False
+            self.statusFalando = True
+            self.statusApresentando = False
+            print(f"{self.nome} está falando sobre sua idade"
+                  f" ({self.idade} anos)")
+            self.statusAtividade = " falando "
     def dormir(self):
-        print(f"{self.nome} foi dormir pelas próximas por 20 horas")
+        if self.statusDormindo==True:
+            print(f"{self.nome} já está dormindo")
+        elif self.statusComendo or self.statusFalando or self.statusApresentando:
+            print(f"{self.nome} não conseguiu, porque está {self.statusAtividade}")
+        else:
+            self.statusComendo=False
+            self.statusDormindo = True
+            self.statusFalando = False
+            self.statusApresentando = False
+            print(f"{self.nome} foi dormir.")
+            self.statusAtividade = " dormindo "
+
 
 
 
